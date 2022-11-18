245fd80ad57cee0bebbc7611221e81c2a3faa489
Holly Reed <2001hollyreed@gmail.com>
2022-11-18 16:22:30 +0000

    Masks directory creation

---
+++ data_preprocessing.py
@@ -0,0 +1,13 @@
+import os
+
+print(os.getcwd())
+# Create directory for masks
+parentDir = 'git/face-segmentation'
+masksDir = 'CelebAMask-HQ-masks'
+path = os.path.join(os.getcwd(), parentDir, masksDir)
+
+try:
+    os.mkdir(path)
+    print('Directory ' + masksDir + ' created')
+except FileExistsError:
+    print('Directory ' + masksDir + ' already exists')
