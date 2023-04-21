8b942c84e36bae834b36cfc5088b5efd7dea3035
Holly Reed <2001hollyreed@gmail.com>
2023-04-21 13:31:17 +0100

    Bug fixes

--- model.py
+++ model.py
@@ -2,8 +2,10 @@ import torch
 import torch.nn as nn
 
 class UNet(nn.Module):
-    def __init__(self, features=32):
+    def __init__(self, features=8):
         super(UNet, self).__init__()
+
+        #self.layers = [in, 8, 16, 32, 64, 128, 256]
 
         self.relu = nn.ReLU()
         self.maxpool = nn.MaxPool2d(kernel_size=2)
@@ -23,23 +25,25 @@ class UNet(nn.Module):
         self.conv1 = nn.Conv2d(in_channels=features*8, out_channels=features*16, kernel_size=3, stride=1, padding='same')
         self.conv2 = nn.Conv2d(in_channels=features*16, out_channels=features*16, kernel_size=3, stride=1, padding='same')
 
-        self.upconv1 = nn.ConvTranspose2d(in_channels=features*16, out_channels=features*16, kernel_size=2, stride=2)
-        self.up1 = nn.Conv2d(in_channels=(features*5)*4, out_channels=features*8, kernel_size=3, stride=1, padding='same')
+        self.upconv1 = nn.ConvTranspose2d(in_channels=features*16, out_channels=features*8, kernel_size=2, stride=2)
+        self.up1 = nn.Conv2d(in_channels=(features*8)*2, out_channels=features*8, kernel_size=3, stride=1, padding='same')
         self.up2 = nn.Conv2d(in_channels=features*8, out_channels=features*8, kernel_size=3, stride=1, padding='same')
 
-        self.upconv2 = nn.ConvTranspose2d(in_channels=features*8, out_channels=features*8, kernel_size=2, stride=2)
-        self.up3 = nn.Conv2d(in_channels=(features*5)*2, out_channels=features*4, kernel_size=3, stride=1, padding='same')
+        self.upconv2 = nn.ConvTranspose2d(in_channels=features*8, out_channels=features*4, kernel_size=2, stride=2)
+        self.up3 = nn.Conv2d(in_channels=(features*4)*2, out_channels=features*4, kernel_size=3, stride=1, padding='same')
         self.up4 = nn.Conv2d(in_channels=features*4, out_channels=features*4, kernel_size=3, stride=1, padding='same')
 
-        self.upconv3 = nn.ConvTranspose2d(in_channels=features*4, out_channels=features*4, kernel_size=2, stride=2)
-        self.up5 = nn.Conv2d(in_channels=features*5, out_channels=features*2, kernel_size=3, stride=1, padding='same')
+        self.upconv3 = nn.ConvTranspose2d(in_channels=features*4, out_channels=features*2, kernel_size=2, stride=2)
+        self.up5 = nn.Conv2d(in_channels=(features*2)*2, out_channels=features*2, kernel_size=3, stride=1, padding='same')
         self.up6 = nn.Conv2d(in_channels=features*2, out_channels=features*2, kernel_size=3, stride=1, padding='same')
 
-        self.upconv4 = nn.ConvTranspose2d(in_channels=features*2, out_channels=features*2, kernel_size=2, stride=2)
-        self.up7 = nn.Conv2d(in_channels=67, out_channels=features, kernel_size=3, stride=1, padding='same')
+        self.upconv4 = nn.ConvTranspose2d(in_channels=features*2, out_channels=features, kernel_size=2, stride=2)
+        self.up7 = nn.Conv2d(in_channels=features*2, out_channels=features, kernel_size=3, stride=1, padding='same')
         self.up8 = nn.Conv2d(in_channels=features, out_channels=features, kernel_size=3, stride=1, padding='same')
 
         self.finalconv = nn.Conv2d(in_channels=features, out_channels=19, kernel_size=1, stride=1)
+
+        #self.layers = [in, 8, 16, 32, 64, 128, 256]
 
     def forward(self, x):
 
@@ -48,54 +52,54 @@ class UNet(nn.Module):
         x1 = self.relu(x1)
         x1 = self.down2(x1)
         x1 = self.relu(x1)
-        x1 = self.maxpool(x1)
+        c1 = self.maxpool(x1)
 
-        x2 = self.down3(x1)
+        x2 = self.down3(c1)
         x2 = self.relu(x2)
         x2 = self.down4(x2)
         x2 = self.relu(x2)
-        x2 = self.maxpool(x2)
+        c2 = self.maxpool(x2)
 
-        x3 = self.down5(x2)
+        x3 = self.down5(c2)
         x3 = self.relu(x3)
         x3 = self.down6(x3)
         x3 = self.relu(x3)
-        x3 = self.maxpool(x3)
+        c3 = self.maxpool(x3)
 
-        x4 = self.down7(x3)
+        x4 = self.down7(c3)
         x4 = self.relu(x4)
         x4 = self.down8(x4)
         x4 = self.relu(x4)
-        x4 = self.maxpool(x4)
+        c4 = self.maxpool(x4)
 
-        x5 = self.conv1(x4)
+        x5 = self.conv1(c4)
         x5 = self.relu(x5)
         x5 = self.conv2(x5)
         x5 = self.relu(x5)
 
         x6 = self.upconv1(x5)
-        x6 = torch.cat((x6, x3), dim=1)
+        x6 = torch.cat((x6, x4), dim=1)
         x6 = self.up1(x6)
         x6 = self.relu(x6)
         x6 = self.up2(x6)
         x6 = self.relu(x6)
 
         x7 = self.upconv2(x6)
-        x7 = torch.cat((x7, x2), dim=1)
+        x7 = torch.cat((x7, x3), dim=1)
         x7 = self.up3(x7)
         x7 = self.relu(x7)
         x7 = self.up4(x7)
         x7 = self.relu(x7)
 
         x8 = self.upconv3(x7)
-        x8 = torch.cat((x8, x1), dim=1)
+        x8 = torch.cat((x8, x2), dim=1)
         x8 = self.up5(x8)
         x8 = self.relu(x8)
         x8 = self.up6(x8)
         x8 = self.relu(x8)
 
         x9 = self.upconv4(x8)
-        x9 = torch.cat((x9, x0), dim=1)
+        x9 = torch.cat((x9, x1), dim=1)
         x9 = self.up7(x9)
         x9 = self.relu(x9)
         x9 = self.up8(x9)
--- train_test.py
+++ train_test.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
     print('Device name:', device)
     
-    BATCH_SIZE = 50
+    BATCH_SIZE = 1
     train = CelebAMask(mode='train')
     data_loader_train = DataLoader(train, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
 
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     loss_func = nn.CrossEntropyLoss()
 
     NUM_EPOCHS = 100
-    LEARNING_RATE = 0.1
+    LEARNING_RATE = 0.01
     MOMENTUM = 0.9
     optim = torch.optim.SGD(model.parameters(), lr = LEARNING_RATE, weight_decay = 0.05, momentum = MOMENTUM)
 
@@ -50,40 +50,47 @@ if __name__ == '__main__':
             labels = labels[:, 0, :, :]
 
             loss = loss_func(outputs, labels.long())
-            #loss.backward()
-            #optim.step()
+            loss.backward()
+            optim.step()
 
             # Accuracy
             pred_y = torch.argmax(outputs, 1)
-            correct += (pred_y == labels).sum()
-            total += float(labels.size(0))
-            total_loss += loss*inputs.shape[0]
+            correct += float((pred_y == labels).sum())
+            total += float(labels.size(0) * labels.size(1) * labels.size(2))
+            total_loss += float(loss*inputs.shape[0])
 
             if (i+1) % 100 == 0:
                 print('Epoch [{}/{}], Iteration [{}/{}], Loss: {:.4f}'.format(epoch + 1, NUM_EPOCHS, i + 1, iterations_per_epoch, loss.item()))
-            
-            correct = 0
-            total = 0
         
         total_loss /= len(train)
-        training_losses.append(total_loss.item())
-        training_accuracies.append(correct/total)
+        training_losses.append(total_loss)
+        training_accuracies.append((correct/total) * 100)
         print('Train accuracy over epoch {}: {:.4f}'.format(epoch + 1, training_accuracies[-1]))
 
         # Evaluation on the test set
         model.eval()
+        total_loss = 0
+        correct = 0
+        total = 0
         with torch.no_grad():
             for inputs, labels in data_loader_test:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                labels[:, 0, :, :] = labels[:, 0, :, :] * 255.0
+                labels = labels[:, 0, :, :]
+
                 outputs = model(inputs)
                 loss = loss_func(outputs, labels.long())
+
                 pred_y = torch.argmax(outputs, 1)
-                correct += (pred_y == labels).sum()
-                total += float(labels.size(0))
-                total_loss += loss*inputs.shape[0]
+                correct += float((pred_y == labels).sum())
+                total += float(labels.size(0) * labels.size(1) * labels.size(2))
+                total_loss += float(loss*inputs.shape[0])
         
         total_loss /= len(test)
-        testing_losses.append(total_loss.item())
-        testing_accuracies.append(correct/total)
+        testing_losses.append(total_loss)
+        testing_accuracies.append((correct/total) * 100)
         model.train()
 
         print('Test accuracy at epoch {}: {:.4f}\n'.format(epoch + 1, testing_accuracies[-1]))
